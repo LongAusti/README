dd50ca633dd274bf8dd5a567342f628b737d0d17
LongAusti <160174081+LongAusti@users.noreply.github.com>
2024-02-22 22:47:35 -0500

    Update lab_assignment_6.c

--- lab_assignment_6/lab_assignment_6.c
+++ lab_assignment_6/lab_assignment_6.c
@@ -8,9 +8,9 @@ int search(int numbers[], int low, int high, int value)
 	if (numbers[mid]==value)
 		return mid;
 	else if (numbers[mid] > value)
-		return search(numbers, low,mid - 1 value);
+		return search(numbers, low,mid - 1 , value);
 	else
-		return search(numbers, mid + 1, high, value);
+		return search(numbers, mid + 1, high , value);
 }
 
 void printArray(int numbers[], int sz)
